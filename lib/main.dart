e02c3c3d564c986989e89c6023db60c196e11dcb
lxw <china2021@126.com>
2018-07-21 10:09:23 +0800

    新增了一个可变的widget，StatefulWidget 用来重写她的方法，返回随机驼峰 名。 在pubspec.yaml 中添加新的 库

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+//    final words = WordPair.random();
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -13,9 +16,28 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color.fromARGB(255, 255, 0, 0),
         ),
         body: Center(
-          child: Text('Hello World'),
+//          child: Text(words.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  //单行函数， 可以使用 => 来简写
+  @override
+  State<StatefulWidget> createState() => new RandomWordsState();
+}
+
+/// 两个类：
+///   StateFul Widget 状态可变
+///   Stateless Widget 状态不可变
+///   实现一个 stateful widget 至少需要两个类：1）一个 StatefulWidget 类；2）一个 State 类
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final WordPair pair = WordPair.random();
+    return new Text(pair.asPascalCase);
   }
 }
